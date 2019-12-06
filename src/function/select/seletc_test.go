447351d60e3b4287cdb72de8039bd4e69508acb4
fuqiang <18251359382@163.com>
2019-12-06 08:59:38 +0800

    test close channel

---
+++ src/function/select/seletc_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("work on something else")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done")
+}
+
+func TestService(t *testing.T) {
+	fmt.Println(service())
+	otherTask()
+}
+
+func AsyncService() chan string {
+	//buffered chan 没有1参数是普通chan
+	retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result")
+		retCh <- ret
+		fmt.Println("service exited")
+	}()
+	return retCh
+}
+
+func TestAsync(t *testing.T) {
+	select {
+	case ret := <-AsyncService():
+		fmt.Println(ret)
+	case <-time.After(time.Millisecond * 10):
+		t.Error()
+	}
+
+}
